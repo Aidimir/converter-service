3eab9a6ed8a37f1706dc41b06861470e2ab5e52f
Aidimir <aidimir1@mail.ru>
2023-05-04 16:37:09 +0500

    Added CORS

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import main_converter
 from json import loads, dumps
 from pathlib import Path
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, Response
 # This is a sample Python script.
 
@@ -16,7 +17,18 @@ from fastapi.responses import FileResponse, Response
 
 # Press the green button in the gutter to run the script.
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def root():
     print("welcome")
